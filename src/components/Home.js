e7947b7dd3bc6ff2bf87ebf5b40b3c33ec8ba12b
unknown <ekpenisiraphael@gmail.com>
2022-12-03 21:51:11 +0530

    Covid status displayed

---
+++ src/components/Home.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+import {
+  faCircleArrowRight,
+  faSearch,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { getCountries } from '../redux/information';
+/* eslint-disable */
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const { countries, status } = useSelector((state) => state.datas);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    if (status === null) {
+      dispatch(getCountries());
+    }
+  });
+
+  return (
+    <div>
+      {status ===
+       'pending' ? (
+        <div className="pending">
+          <h3> Loadings...</h3>
+        </div>
+      ) : (
+        <>
+          <div className="search">
+            <div className='search_element'>
+            <h1 className="live_preview">Live Preview</h1>
+            <input
+              type="text"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder="search"
+              className="input"
+            />
+            <FontAwesomeIcon icon={faSearch} className="search_icon" />
+            </div>
+          </div>
+          <div className="display_flex">
+            {countries
+              .filter(
+                (searchCountry) => searchCountry.continent
+                  .toLowerCase()
+                  .includes(search.toLowerCase())
+                || searchCountry.continent
+                  .toLowerCase()
+                  .includes(search.toLowerCase()),
+              ).map((item) => (
+                <div key={uuidv4()} className="country_display">
+                  <NavLink state={item} to="/details">
+                    <FontAwesomeIcon
+                      icon={faCircleArrowRight}
+                      className="arrow-right"
+                    />
+                  </NavLink>
+                  <div className="update_countries">
+                    <h3>{item.continent}</h3>
+                    <p>{item.updated}</p>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Home;
